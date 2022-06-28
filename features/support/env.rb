e173afcadf0c57bb52cc60ef212a383d120b864f
pshivapr <shivaprasad.pasham@gmail.com>
2022-06-28 15:23:33 +0100

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,5 @@
 require 'capybara/cucumber'
+require 'webdrivers'
 require 'selenium-webdriver'
 require 'capybara-screenshot/cucumber'
 require 'axe-cucumber-steps'
@@ -7,7 +8,4 @@ FileUtils.remove_dir("results") if File.directory?("results")
 FileUtils.mkdir_p("results") if not File.directory?("results")
 
 Capybara.save_path = "results/screenshot"
-
-Selenium::WebDriver::Chrome::Service.driver_path="/Users/shivapasham/Downloads/resources/chromedriver"
-
 Capybara.default_driver = :selenium_chrome
